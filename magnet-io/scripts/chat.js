403e0ddb212edd6f8f615cd57e72226dd79b117d
BStachera <bartlomiejstachera@gmail.com>
2018-11-11 22:36:50 +0100

    cactujam 11.2018

--- magnet-io/scripts/chat.js
+++ magnet-io/scripts/chat.js
@@ -2,9 +2,17 @@ function chatinfo(x){
     if( x == 1){
         document.getElementById('chat').innerHTML = chat_message[1];
         document.getElementById('magnetio').src="images/magnet/2.png";
+        energy += 10;
+        document.getElementById('energy_cnt').innerHTML = energy;
+        document.getElementById('checkerp').style.visibility = "hidden";
+        document.getElementById('checkerm').style.visibility = "hidden";
+
     } else if( x == 0){
         document.getElementById('chat').innerHTML = chat_message[2];
-        document.getElementById('magnetio').src="images/magnet/0.png";
+        document.getElementById('magnetio').src="images/magnet/0.png";      
+        document.getElementsByClassName('btn')[0].style.visibility = "hidden";
+        document.getElementsByClassName('btn')[1].style.visibility = "hidden";
+        setTimeout("location.href = 'https://www.thespruceeats.com/thmb/baY0uvKFvdBu2EGMy8mQiPyvbkA=/3315x2483/filters:no_upscale():max_bytes(150000):strip_icc()/prickly-pear-cactus-growing-against-sky-680876127-582c787c5f9b58d5b1b4924f.jpg';",5000);
     }else{
         console.log("error kurde");
     }
--- magnet-io/scripts/script.js
+++ magnet-io/scripts/script.js
@@ -1,9 +1,9 @@
 let chat_message = [
     "HELLO I’M MAGNETIO I REALLY WHAT TO BE THE MOST POWERFULL MAGNET IN THE UNIVERSE CAN YOU HELP ME?",
-    "REALLY? I'M SO EXCITED. I WILL HELP YOU :D LET'S START FROM BUING A NEWSPAPER TO CHARGE ME UP!",
-    "WHY? I THOUGHT THAT IT WILL BE FUN.. EHH.... OKAY SO.. I WILL TELL EVERYBODY ON CACTUS THAT YOU ARE BAD PERSON :/ FEELS DEMAGNETIC",
-    "WHAT ELECTRONS CAN PAPER ARCHIWE WHEN IT IS SCRATCHING THE GLASS BOTTLE? PLUS OR MINUS? YOU CAN WON 10X MORE ENEGRY :3",
-    "YOU CAN BUY A SOCK, WOOL SOCK WITH A LOT OD ELECTRONS",
+    "REALLY? I'M SO EXCITED. I WILL HELP YOU :D LET'S START FROM BUING A NEWSPAPER AND MAGNETIZING ATOMS AT THE LEFT! HERE IS <br> 10+ energy :3",
+    "WHY? I THOUGHT THAT IT WILL BE FUN.. EHH.... OKAY SO.. GTFO ON CACTUS :/ FEELS DEMAGNETIC",
+    "WHAT ELECTRONS CAN PAPER ARCHIWE WHEN IT IS SCRATCHING THE GLASS BOTTLE? PLUS OR MINUS? YOU CAN WON<br> 10x MORE ENEGRY :3",
+    "SOFT SOCK FOR REAL GAMERS AND MAGNETIONS, CACTUSOCK CACTU SUCKS, JUST KIDDING",
     "OOOO HELLO EMO GUY, YOU WILL HELP ME WITH YOUR AMAZING HAIR FROM METALIC MUSIC",
     "I'M GLAD TO MEET A DEAD ZEBER",
     "LET'S WORK WITH A HAND HUHHU IT'S LIKE MASTURBATION BUT YOU WILL HELP ME NOT YOU ;*",
@@ -12,7 +12,7 @@ let chat_message = [
     "CACTU GENERATOR, YOU ARE SERIOUS, WAIT..... OOO..... WHAT????.... THIS IS SOOOOO POWERFULLLLLLLLLL",
     "I WILL BE LIKE A CACTU GOD, CACTU GOD IS THE MOST POWERFUL THING WHAT CAN YOU IMAGINE AND USE"
 ];
-let energy = 10;
+let energy = 0;
 let item_cnt = [0,0,0,0,0,0,0,0,0];
 let item_add = [1,10,50,100,500,1000,5000,100000,500000];
 let item_act = [0,0,0,0,0,0,0,0,0];
@@ -33,14 +33,58 @@ function buy(x){
         energy -= item_prc[x];
         item_cnt[x] += 1;
         document.getElementById('energy_cnt').innerHTML = energy;
-        document.getElementById('count').innerHTML = "COUNT: "+item_cnt[x];
+        document.getElementsByClassName('count')[x].innerHTML = "COUNT: "+item_cnt[x];
+
+        if(item_cnt[0] == 1){
+            document.getElementById('chat').innerHTML = chat_message[3];
+            document.getElementsByClassName("btn")[0].id = "plus";
+            document.getElementsByClassName("btn")[1].id = "minus";
+            document.getElementsByClassName('btn')[0].style.visibility = "hidden";
+            document.getElementsByClassName('btn')[1].style.visibility = "hidden";
+            document.getElementsByClassName('btn')[0].onclick = "glosowanie_one(1)";
+            document.getElementsByClassName('btn')[1].onclick = "glosowanie_one(0)";
+        }
+        if(item_cnt[1] == 1){
+            document.getElementById('chat').innerHTML = chat_message[4];
+        }
+        if(item_cnt[2] == 1){
+            document.getElementById('chat').innerHTML = chat_message[5];
+        }
+        if(item_cnt[3] == 1){
+            document.getElementById('chat').innerHTML = chat_message[6];
+        }
+        if(item_cnt[4] == 1){
+            document.getElementById('chat').innerHTML = chat_message[7];
+        }
+        if(item_cnt[5] == 1){
+            document.getElementById('chat').innerHTML = chat_message[8];
+        }
+        if(item_cnt[6] == 1){
+            document.getElementById('chat').innerHTML = chat_message[9];
+        }
+        if(item_cnt[7] == 1){
+            document.getElementById('chat').innerHTML = chat_message[10];
+        }
+        if(item_cnt[8] == 1){
+            document.getElementById('chat').innerHTML = chat_message[11];
+        }
+
     }else{
         console.log("bieda");
     }
 }
 function table_create(){
     for( let x=1; x<= 9; x++){
-        document.write("<li class='items'> <ul class='itembar'><li> <img src='images/item/"+x+".gif'></li><li> <span class='buy' onclick='buy("+(x-1)+")'>BUY</span></li><li> <span id='count'>COUNT:"+item_cnt[x-1]+" </span></li><li> <span>"+nazwy[x-1]+"</span></li><li> <span id='price'>"+item_prc[x-1]+"<img class='powerprice' src='images/power.png'></span></li></ul></li>")
+        document.write("<li class='items'> <ul class='itembar'><li> <img src='images/item/"+x+".gif'></li><li> <span class='buy' onclick='buy("+(x-1)+")'>BUY</span></li><li> <span class='count'>COUNT:"+item_cnt[x-1]+" </span></li><li> <span>"+nazwy[x-1]+"</span></li><li> <span id='price'>"+item_prc[x-1]+"<img class='powerprice' src='images/power.png'></span></li></ul></li>")
     }
     document.getElementById('chat').innerHTML = chat_message[0];
+}
+function glosowanie_one(x){
+
+}
+function glosowanie_one(){
+
+}
+function glosowanie_one(){
+
 }
